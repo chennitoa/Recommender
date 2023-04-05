c0955aae643b414133c41db3379f7a319ecc20f2
chennitoa <achennito@gmail.com>
2023-04-04 21:14:27 -0700

    Added login page controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,46 +1,24 @@
 package application;
 	
-import gui.LoginPage;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			VBox loginRoot = FXMLLoader.load(getClass().getResource("../gui/LoginScreen.fxml"));
+	        Scene loginScene = new Scene(loginRoot);
 			
-			MoveTo startPoint = new MoveTo(108, 71);
-			LineTo line1 = new LineTo(321,161);
-			LineTo line2 = new LineTo(126,232);
-			LineTo line3 = new LineTo(232,52);
-			LineTo line4 = new LineTo(269,250);
-			LineTo line5 = new LineTo(108,71);
-			
-			Path path = new Path();
-			path.getElements().addAll(startPoint, line1, line2, line3, line4, line5);
-			Button sceneChangeButton = new Button("Change Scene");
-			
-			Group root = new Group(path, sceneChangeButton);
-			Scene scene = new Scene(root, 400, 300);
-			
-			LoginPage loginPage = new LoginPage();
-			Scene loginScene = loginPage.getLoginPage();
-			
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			sceneChangeButton.setOnAction(event -> {
-				primaryStage.setScene(loginScene);
-			});
-			
-		} catch(Exception e) {
+			loginScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(loginScene);
+			primaryStage.show();	
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
