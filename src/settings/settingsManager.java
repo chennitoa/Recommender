7a9b991ad7ed31cc41b6bab4f0ef86538dbea264
aokj08 <104790843+aokj08@users.noreply.github.com>
2023-04-08 14:38:11 -0700

    Update settingsManager.java

--- src/settings/settingsManager.java
+++ src/settings/settingsManager.java
@@ -1,25 +1,32 @@
 package settings;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class settingsManager {
 	
-	private List<String> semesters = Arrays.asList("Spring" , "Fall", "Summer");
-	private List<String> courses = Arrays.asList("CS151: Object-Oriented Design","CS166: Information Security",
-			"CS154: Theory of Computation","CS160: Software Engineering","CS256: Cryptography","CS146: Data Structures and Algorithms",
-			"CS152: Programming Languages Paradigm");
-	private List<String> programs = Arrays.asList("Master of science (MS)","Master of business administration (MBA)",
-			"Doctor of philosophy (PhD)");
-	private List<String> personalCharacteristics = Arrays.asList("very passionate","very enthusiastic","punctual","attentive","polite");
-	private List<String> academicCharacteristics = Arrays.asList("submitted well-written asisgnments", "participated in all of my class activities",
-			"worked hard", "was very well prepared for every exam and assignment", "picked up new skills quickly",
-			"was able to excel academically at the top of my class");
+	private List<String> semesters = new ArrayList<>();
+	private List<String> courses = new ArrayList<>();
+	private List<String> programs = new ArrayList<>();
+	private List<String> personalCharacteristics = new ArrayList<>();
+	private List<String> academicCharacteristics = new ArrayList<>();
 	
 	/*
-	 * Single design
+	 * Singleton design
 	 */
-	private settingsManager() {}
+	private settingsManager() {
+		semesters = Arrays.asList("Spring" , "Fall", "Summer");
+		courses = Arrays.asList("CS151: Object-Oriented Design","CS166: Information Security",
+				"CS154: Theory of Computation","CS160: Software Engineering","CS256: Cryptography","CS146: Data Structures and Algorithms",
+				"CS152: Programming Languages Paradigm");
+		programs = Arrays.asList("Master of science (MS)","Master of business administration (MBA)",
+				"Doctor of philosophy (PhD)");
+		personalCharacteristics = Arrays.asList("very passionate","very enthusiastic","punctual","attentive","polite");
+		academicCharacteristics = Arrays.asList("submitted well-written asisgnments", "participated in all of my class activities",
+				"worked hard", "was very well prepared for every exam and assignment", "picked up new skills quickly",
+				"was able to excel academically at the top of my class");
+	}
 	
 	/*
 	 * Returns a professor java bean with initial data
